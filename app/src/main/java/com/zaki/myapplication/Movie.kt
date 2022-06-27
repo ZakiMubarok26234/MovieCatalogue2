bf9973eac6a5c451a8cf38f4bbbaa21b9ea207d5
Fadli000 <fadlihasibuan97@gmail.com>
2022-06-27 14:44:32 +0700

    Initial commit

---
+++ app/src/main/java/com/zaki/myapplication/Movie.kt
@@ -0,0 +1,29 @@
+package com.zaki.myapplication
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+    data class Movie(
+        @SerializedName("id")
+        val id : String ?,
+
+        @SerializedName("title")
+        val title : String?,
+
+        @SerializedName("poster_path")
+        val poster : String?,
+
+        @SerializedName("release_date")
+        val release : String?,
+
+        @SerializedName("overview")
+        val overview: String?,
+
+        @SerializedName("genre_ids")
+        val genre_ids: String?,
+    ) : Parcelable {
+        constructor() : this("", "", "", "","","")
+    }
+
